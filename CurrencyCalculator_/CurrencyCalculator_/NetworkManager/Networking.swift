b9a07655c7640914079d1817c591237f44bd89ee
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-07-13 12:23:25 +0100

    updated

--- CurrencyCalculator_/CurrencyCalculator_/NetworkManager/Networking.swift
+++ CurrencyCalculator_/CurrencyCalculator_/NetworkManager/Networking.swift
@@ -12,7 +12,7 @@ import SwiftyJSON
 class FetchCurrency {
     func fetchConversionRate() {
         let apiKey = "b5528a9e7fb5b4ee0ff0025199583441"
-        let request = AF.request("https://data.fixer.io/api/latest?access_key=b5528a9e7fb5b4ee0ff0025199583441")
+        let request = AF.request("http://data.fixer.io/api/latest?access_key=\(apiKey)")
         request.responseJSON { (data) in
           print(data)
         }
